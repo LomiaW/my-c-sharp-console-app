693951d6e38f09b7971d5e572c66e490180fc586
Lomia Wu <rwu30@myseneca.ca>
2023-02-21 14:54:06 -0500

    init MySimpleBank App

---
+++ MySimpleBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MySimpleBank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var account = new BankAccount("Mia", 10000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+      
+      account.MakeWithdrawal(2500, DateTime.Now, "Rent payment");
+      Console.WriteLine(account.Balance);
+      
+      account.MakeDeposit(500, DateTime.Now, "Jackpot");
+      Console.WriteLine(account.Balance);
+
+      Console.WriteLine(account.GetAccountHistory());
+    }
+  }
+}
